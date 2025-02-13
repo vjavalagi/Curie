3b9d3b32dc8d1ba5c4d91b3e603f873d1a4e8322
Joshua Mayhugh <123776276+jmayhugh1@users.noreply.github.com>
2025-02-13 14:54:04 -0600

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-sub">
+                <div className="he">Logo</div>
+            </div>
+            <div className="header-sub">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." />
+                </div>
+            </div>
+            <div className="header-sub">
+                <div className="profile">Profile</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
