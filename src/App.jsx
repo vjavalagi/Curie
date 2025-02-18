6f3b10cffae0ddc534fd076410cd66c768fd57f1
Shyam Ramachandran <janshyam03@gmail.com>
2025-02-18 13:32:13 -0600

    adding router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import LandingPage from './Pages/LandingPage'
+import Home from './Pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <Header/>
+      <Header/>
+
+      
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path ='*' element={<h1>Not Found</h1>} />
+      </Routes>
+      
 
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </BrowserRouter>
   )
 }
 
