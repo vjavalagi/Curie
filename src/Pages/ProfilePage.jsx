89a91990d0469722370732249e086b11264e0ca3
vjavalagi <vedajavalagi@tamu.edu>
2025-02-20 17:07:58 -0600

    Folder rectangles to Icon

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import DirectoryDropdown from "../components/DirectoryDropdown";
 import FolderGrid from "../components/FolderGrid";
@@ -6,6 +6,8 @@ import SaveGroupingButton from "../components/SaveGroupingButton";
 import WelcomeMessage from "../components/WelcomeMessage";
 
 export default function ProfilePage() {
+  const [folders, setFolders] = useState(["Folder 1", "Folder 2", "Folder 3", "Folder 4", "Folder 5", "Folder 6", "yasss", "yay", "yipee"]); // Dummy folder names
+
   return (
     <div className="h-screen flex flex-col bg-gray-50">
       <Header variant="lightblue" />
@@ -13,9 +15,13 @@ export default function ProfilePage() {
         <div className="w-1/4 p-4">
           <DirectoryDropdown />
         </div>
-        <div className="flex flex-col items-center justify-center flex-1">
-          <WelcomeMessage />
-          <FolderGrid />
+        <div className="flex flex-col flex-1 items-center">
+          {/* Center the WelcomeMessage at the top */}
+          <div className="text-center mt-6">
+            <WelcomeMessage />
+          </div>
+          
+          <FolderGrid folders={folders} />
         </div>
       </div>
       <SaveGroupingButton />
