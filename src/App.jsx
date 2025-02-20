1e4dbb28532336417a6392f4bd47bea7d9d18e96
Joshua Mayhugh <123776276+jmayhugh1@users.noreply.github.com>
2025-02-20 12:07:04 -0600

    removing stuff from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import LandingPage from './Pages/LandingPage'
-import Home from './Pages/Home'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+
+import "./App.css";
+import Header from "./components/Header";
+import LandingPage from "./Pages/LandingPage";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <div>
-      <Header/>
+        <Header />
 
-      
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/home" element={<h1>Home</h1>} />
-        <Route path ='*' element={<h1>Not Found</h1>} />
-      </Routes>
-      
-
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
       </div>
-      </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
