94853dd5d3583652c51921eac9148bc44a70ae0d
vjavalagi <vedajavalagi@tamu.edu>
2025-03-05 10:55:43 -0600

    Sizing fix for download button

--- src/components/SearchLargeView.jsx
+++ src/components/SearchLargeView.jsx
@@ -51,9 +51,9 @@ export default function SearchLargeView() {
         </div>
         <button
             type="button"
-            className="flex justify-center items-center size-11 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+            className="flex justify-center items-center px-4 py-2 min-w-[150px] text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
             onClick={handlePdfDownloadClick}
-            disabled={!activePaper?.openAccessPdf?.url || isDownloading} // ✅ Disable if no URL or downloading
+            disabled={!activePaper?.openAccessPdf?.url || isDownloading} 
           >
             {isDownloading ? (
               <span
