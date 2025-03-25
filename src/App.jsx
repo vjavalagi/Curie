a83ca86a18d8c3df43f8bf78db52dda03d9d5873
sydneylipar <145057271+sydneylipar@users.noreply.github.com>
2025-03-25 12:32:05 -0500

    Login (#51) Full Login and Create Account UI
    * First draft frontend Login Page
    
    * Revert "First draft frontend Login Page"
    
    This reverts commit 2936bc2fdc57920cb60532d69da3d64eebb225d2.
    
    * First draft Login.jsx frontend
    
    * Create Account file frontend
    
    * Full UI for login and create account

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-
 import Header from "./components/Header";
 import LandingPage from "./Pages/LandingPage";
 import Profile from "./Pages/ProfilePage";
+import Login from "./Pages/Login";
+import CreateAccount from "./Pages/CreateAccount";
 import SearchPage from "./Pages/SearchPage";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
@@ -18,6 +19,8 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/createaccount" element={<CreateAccount />} />
             <Route path="*" element={<h1>Not Found</h1>} />
           </Routes>
         </GlobalProvider>
